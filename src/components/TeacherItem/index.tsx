17a0cb3844001bb880e9cbad829ead6c09d09c03
eupolemo.castro <eupolemo.castro@dataprev.gov.br>
2020-08-08 10:01:02 -0300

    Aula 3 - integrando frontend com backend.

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,33 +3,51 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+export interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
     return (
         <article className="teacher-item">
                     <header>
-                        <img src="https://avatars0.githubusercontent.com/u/15064935?s=400&v=4" alt="Eupólemo Castro"/>
+                        <img src={teacher.avatar} alt={teacher.name} />
                         <div>
-                            <strong>Eupólemo Castro</strong>
-                            <span>Química</span>
+                            <strong>{teacher.name}</strong>
+                            <span>{teacher.subject}</span>
                         </div>
                     </header>
 
-                    <p>
-                        Entusiasta das melhores tecnologias de química avançada.
-                        <br/><br/>
-                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-                    </p>
+                    <p>{teacher.bio}</p>
 
                     <footer>
                         <p>
                             Preço/hora
-                            <strong>R$ 80,00</strong>
+                            <strong>R$ {teacher.cost}</strong>
                         </p>
-                        <button type="button">
-                            <img src={whatsappIcon} alt="Whatsapp"/>
+                        <a
+                            target="_blank"
+                            onClick={createNewConnection}
+                            href={`https://wa.me/${teacher.whatsapp}`}>
+                            <img src={whatsappIcon} alt="Whatsapp" />
                             Entrar em contato.
-                        </button>
+                        </a>
                     </footer>
                 </article>
     );
